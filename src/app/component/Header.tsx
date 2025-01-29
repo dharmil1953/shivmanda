d184c4b05f64eeb6f17d72eea66a4fe07b91e134
dharmil <dharmilkotak00@gmail.com>
2025-01-29 23:26:08 +0530

    additional

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import logo from "../../../public/images/logo.svg";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { Menu, X } from "lucide-react"; // Hamburger and close icons
+import { Menu, X } from "lucide-react"; 
 
 const navLinks = [
   { href: "/", label: "Home" },
